e24edcb17764951bf1f255932384c34fd71f0311
mehulsatardekar <mehulsatardekar1999@gmail.com>
2021-09-06 20:23:49 +0530

    added images based on loss/profit and footer and calculation changes

--- index.js
+++ index.js
@@ -3,29 +3,34 @@ const stockquantityElem = document.querySelector('#stockquantity')
 const currentpriceElem = document.querySelector('#currentprice')
 const resultBtn = document.querySelector('#calres')
 const result = document.querySelector('.result')
+const bgimg = document.querySelector('.img');
+const sadUrl = 'https://thumbs.dreamstime.com/b/children-boy-thinking-smile-idea-chalkboard-cute-kid-imagine-classroom-space-your-text-education-concept-books-102349052.jpg';
+const happyUrl = 'https://img.freepik.com/free-vector/happy-young-couple-having-fun-girl-guy-dancing-party-celebrating-good-news-flat-illustration_74855-10820.jpg?size=338&ext=jpg';
 resultBtn.addEventListener('click',()=>{
 
-    const initialPrice = Number(initialpriceElem.value);
-    const stockquantity = Number(stockquantityElem.value);
-    const currentprice = Number(currentpriceElem.value);
-    let res=0, percentage=0;
+    const init_price = Number(initialpriceElem.value);
+    const quantity = Number(stockquantityElem.value);
+    const curr_price = Number(currentpriceElem.value);
 
-    if(initialPrice>0 && stockquantity>0 && currentprice>0){
-    if(currentprice>initialPrice){
-        res = (currentprice - initialPrice) *stockquantity;
-        percentage = (res/initialPrice) *100;
-        result.innerText =`Your profit is ${res} and profit percentage is ${percentage.toFixed(3)}%`;
+    if(init_price>0 && quantity>0 && curr_price>0){
+    if(init_price > curr_price){
+        let loss = ((init_price - curr_price) * quantity).toFixed(2);
+        let loss_per = ((loss / (init_price*quantity)) * 100).toFixed(2);
+        result.innerText =`Your loss is ${loss} and loss percentage is ${loss_per}%`;
+        bgimg.innerHTML = `<img src=${sadUrl} class='sadimg' />`
         result.classList.add('success');
-    }else if(initialPrice> currentprice){
-        res = (initialPrice - currentprice) *stockquantity;
-        percentage = (res/ initialPrice) *100;
-        result.innerText = `Your loss is ${res} and loss percentage is ${percentage.toFixed(3)}%`;
-        result.classList.add('danger');
+    }else if(init_price < curr_price){
+        let profit = ((curr_price - init_price) * quantity).toFixed(2);
+        let profit_per = ((profit / (init_price*quantity)) * 100).toFixed(2);
+        result.innerText = `Your profit is ${profit} and profit percentage is ${profit_per}%`;
+        bgimg.innerHTML = `<img src=${happyUrl} class='sadimg' />`
+        result.classList.add('success');
     }else{
         result.innerText=`No Loss & No Profit`;
     }
   }else{
       result.innerText=`All values Must Be Greater than Zero`;
+      bgimg.innerText='';
   }
 
 })
